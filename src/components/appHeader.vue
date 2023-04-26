32e69eac057a6fcfbc06300891bdf179a9302d1e
RobertoNesta <robertonesta1993@gmail.com>
2023-04-26 13:00:56 +0200

    add socialMenu component in appHeader

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,9 +1,15 @@
 <template>
-    
+    <header>
+        <socialMenu></socialMenu>
+    </header>
 </template>
 
 <script>
+import socialMenu from "./socialMenu.vue"
 export default {
+    components: {
+        socialMenu
+    },
     name: "appHeader",
     setup () {
         return {}
---
+++ src/components/socialMenu.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    name: "socialMenu",
+    setup () {
+        return {}
+    }
+}
+</script>
+
+<template>
+    <div class="container d-flex justify-content-between align-items-center">
+        <div class="socials_left d-flex justify-content-between align-items-center">
+            <ul class=" list-unstyled gap-3 my-3 d-flex justify-content-between align-items-center">
+                <li><a class="text-decoration-none text-dark" href="#">Download App </a>
+                <button class="text-light btn">iOS</button></li>
+                <li><a class="text-decoration-none text-dark" href="#">Advertise with us</a></li>
+            </ul>
+        </div>
+        <div class="socials_right">
+            <ul class="d-flex list-unstyled gap-3 my-3">
+                <li><a href="#"></a><i class="fa-brands fa-facebook"></i></li>
+                <li><a href="#"></a><i class="fa-brands fa-instagram"></i></li>
+                <li><a href="#"></a><i class="fa-brands fa-twitter"></i></li>
+                <li><a href="#"></a><i class="fa-brands fa-youtube"></i></li>
+            </ul>
+            
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container{
+    button{
+        padding: 0.125rem 0.5rem;
+        background-color: #fc7525;
+        border: none;
+    };
+    i{
+        color: grey;
+    }
+}
+
+</style>
