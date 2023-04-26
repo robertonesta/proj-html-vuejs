5bdf01adfbfea5b1d88b0d02f0be0d4bc0448be1
RobertoNesta <robertonesta1993@gmail.com>
2023-04-26 20:50:01 +0200

    culinary collection component added

--- src/components/culinaryCollection.vue
+++ src/components/culinaryCollection.vue
@@ -16,38 +16,61 @@ export default {
             <span>Aliquam erat volutpat. Ut quis ligula a magna blandit finibus. Suspendisse maximus lacus non nunc lacinia lobortis.</span>
         </div>
         <div class="d-flex justify-content-around py-1">
-            <div class="row">
-                <div class="col-3 py-1">
-                    <img src="../assets/img/drinks-recipes.png" alt="">
-                    <span class="text-uppercase">drinks</span>
+            <div class="row row-cols-4">
+                <div class="col px-2 py-1">
+                    <div class="box">
+                        <img src="../assets/img/drinks-recipes.png" alt="">
+                        <span class="text-uppercase">drinks</span>
+                    </div>
                 </div>
-                <div class="col-3 py-1">
-                    <img src="../assets/img/soups-recipes.png" alt="">
-                    <span class="text-uppercase">soup</span>
+                <div class="col px-2 py-1">
+                    <div class="box">
+
+                        <img src="../assets/img/soups-recipes.png" alt="">
+                        <span class="text-uppercase">soup</span>
+                    </div>
                 </div>
-                <div class="col-3 py-1">
-                    <img src="../assets/img/baking-recipes.png" alt="">
-                    <span class="text-uppercase">bakery</span>
+                <div class="col px-2 py-1">
+                    <div class="box">
+
+                        <img src="../assets/img/baking-recipes.png" alt="">
+                        <span class="text-uppercase">bakery</span>
+                    </div>
                 </div>
-                <div class="col-3 py-1">
-                    <img src="../assets/img/dinner-recipes.png" alt="">
-                    <span class="text-uppercase">dinner</span>
+                <div class="col px-2 py-1">
+                    <div class="box">
+                        <img src="../assets/img/dinner-recipes.png" alt="">
+                        <span class="text-uppercase">dinner</span>
+                    </div>
+
                 </div>
-                <div class="col-3 py-1">
-                    <img src="../assets/img/healthy-recipes.png" alt="">
-                    <span class="text-uppercase">healthy</span>
+                <div class="col px-2 py-1">
+                    <div class="box">
+                        <img src="../assets/img/healthy-recipes.png" alt="">
+                        <span class="text-uppercase">healthy</span>
+
+                    </div>
                 </div>
-                <div class="col-3 py-1">
-                    <img src="../assets/img//staff-picks.png" alt="">
-                    <span class="text-uppercase">staff picks</span>
+                <div class="col px-2 py-1">
+                    <div class="box">
+                        <img src="../assets/img//staff-picks.png" alt="">
+                        <span class="text-uppercase">staff picks</span>
+
+                    </div>
                 </div>
-                <div class="col-3 py-1">
-                    <img src="../assets/img/premium-recipes.png" alt="">
-                    <span class="text-uppercase">appetisers</span>
+                <div class="col px-2 py-1">
+                    <div class="box">
+                        <img src="../assets/img/premium-recipes.png" alt="">
+                        <span class="text-uppercase">appetisers</span>
+
+                    </div>
                 </div>
-                <div class="col-3 py-1">
-                    <img src="../assets/img/quick-easy-recipes.png" alt="">
+                <div class="col px-2 py-1">
+                    <div class="box">
+                        <img src="../assets/img/quick-easy-recipes.png" alt="">
                     <span class="text-uppercase">quick and easy</span>
+                    </div>
+                    
                 </div>
             </div>
         </div>
@@ -55,15 +78,18 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.col-3{
-    background-color: white;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    img{
-        margin: 0 auto;
-        width: 100px;
+.col{    
+    .box{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        background-color: white;
+        img{
+            margin: 0 auto;
+            width: 100px;
+        }
     }
 }
+    
 </style>
