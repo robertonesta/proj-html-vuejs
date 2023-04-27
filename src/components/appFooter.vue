026991683d270c61d511a45979aaa1f43d92342f
RobertoNesta <robertonesta1993@gmail.com>
2023-04-27 13:31:21 +0200

    creation of footerTop and footerBottom components

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -1,10 +1,15 @@
 <template>
+    <footerTop/>
+    <footerBottom/>
     
 </template>
 
 <script>
+import footerTop from './footerTop.vue';
+import footerBottom from './footerBottom.vue';
 export default {
     name: "appFooter",
+    components: "footerTop, footerBottom",
     setup () {
         
 
---
+++ src/components/footerBottom.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "footerBottom",
+    setup () {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
